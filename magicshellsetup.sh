efa1d4b4f7ae70148e84eb371c265320565df256
georgewoodall82 <47471634+georgewoodall82@users.noreply.github.com>
2021-10-03 21:52:30 +0100

    Add files via upload

---
+++ magicshellsetup.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/georgewoodall82/magic-shell
+cd magic-shell
+wget https://bootstrap.pypa.io/get-pip.py
+python ./get-pip.py
+pip install json
+pip install requests
+pip install os
+echo Installed! Use 'python ./Magic-Shell.py' to run.
