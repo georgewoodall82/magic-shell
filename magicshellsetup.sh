d4720d5575b382ac285425c45ca2644e13073096
georgewoodall82 <47471634+georgewoodall82@users.noreply.github.com>
2021-10-04 07:25:56 +0100

    Update magicshellsetup.sh

--- magicshellsetup.sh
+++ magicshellsetup.sh
@@ -1,7 +1,7 @@
 git clone https://github.com/georgewoodall82/magic-shell
 cd magic-shell
 wget https://bootstrap.pypa.io/get-pip.py
-sudo python3 ./get-pip.py
+python3 ./get-pip.py
 pip install json
 pip install requests
 pip install os
